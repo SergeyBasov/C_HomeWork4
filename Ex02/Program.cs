3f70aa9039e10dc2ccb755d1daf582cb0fd3952c
Сергей <basov888@ya.ru>
2023-01-30 17:07:33 +0700

    New project HomeWork4 Ex02

---
+++ Ex02/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число 
+и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+void SumNumber(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result = result + num % 10;
+        num = num / 10;
+    }
+    Console.WriteLine($" -> {result} (сумма цифр введеного числа)");
+}
+
+int GetNum(string text)
+{
+    Console.Write(text);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int num = GetNum("Введите число : ");
+SumNumber(num);
