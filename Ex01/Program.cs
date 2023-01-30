6765e696ef1215cb97c268a74f25126d81b862ec
Сергей <basov888@ya.ru>
2023-01-30 17:03:10 +0700

    New project HomeWork4 Ex01

---
+++ Ex01/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два
+числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+double GetNum(string text)
+{
+    Console.Write(text);
+    double num = double.Parse(Console.ReadLine());
+    return num;
+}
+double numA = GetNum("Введите число A: ");
+double numB = GetNum("Введите число B: ");
+
+double result = Math.Pow(numA, numB);
+Console.WriteLine($" -> {result} ({numA} в степени {numB})");
